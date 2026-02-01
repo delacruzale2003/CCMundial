93a16b87fe85d598809b0143b41d2809ea8bb1c8
Alejandro <delacruzcalderonalejandro23@gmail.com>
2026-01-31 23:16:45 -0500

    upload image processing improved for performance

--- src/constants/RegistrationConstants.tsx
+++ src/constants/RegistrationConstants.tsx
@@ -8,4 +8,5 @@ export interface RouteParams extends Record<string, string | undefined> {
 // Variables de Configuración (simulando que vienen de .env o Vite)
 export const CAMPAIGN_ID = import.meta.env.VITE_CAMPAIGN;
 export const API_URL = import.meta.env.VITE_API_URL;
-export const UPLOAD_URL = "https://ptm.pe/PremiosApp/upload.php";
+export const UPLOAD_URL = "https://ptm.pe/PremiosApp/upload_fixed.php";
+//export const UPLOAD_URL = "https://ptm.pe/PremiosApp/upload.php";
